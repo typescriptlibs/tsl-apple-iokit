4c72e2fe45a624e90b0ac0d5573d689be19a8615
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2023-08-28 20:45:04 +0200

    Add Plane enum

---
+++ lib/Plane.d.ts
@@ -0,0 +1,21 @@
+/*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*\
+
+  Apple I/O Kit TypeScript Library
+
+  Copyright (c) TypeScriptLibs and Contributors
+
+  Licensed under the MIT License.
+  You may not use this file except in compliance with the License.
+  You can get a copy of the License at https://typescriptlibs.org/LICENSE.txt
+
+\*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*/
+export declare enum Plane {
+    ACPI = "IOACPIPlane",
+    CoreCapture = "CoreCapture",
+    DeviceTree = "IODeviceTree",
+    Power = "IOPower",
+    Service = "IOService",
+    WLAN = "IO80211Plane",
+    USB = "IOUSB"
+}
+export default Plane;
---
+++ src/Plane.ts
@@ -0,0 +1,39 @@
+/*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*!*\
+
+  Apple I/O Kit TypeScript Library
+
+  Copyright (c) TypeScriptLibs and Contributors
+
+  Licensed under the MIT License.
+  You may not use this file except in compliance with the License.
+  You can get a copy of the License at https://typescriptlibs.org/LICENSE.txt
+
+\*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*i*/
+
+
+/* *
+ *
+ *  Enum
+ *
+ * */
+
+
+export enum Plane {
+    ACPI = 'IOACPIPlane',
+    CoreCapture = 'CoreCapture',
+    DeviceTree = 'IODeviceTree',
+    Power = 'IOPower',
+    Service = 'IOService',
+    WLAN = 'IO80211Plane',
+    USB = 'IOUSB',
+}
+
+
+/* *
+ *
+ *  Default Export
+ *
+ * */
+
+
+export default Plane;
